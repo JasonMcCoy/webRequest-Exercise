da32e579717c755a451040c972dde2bb849723ed
Jason McCoy <jmccoy@JMcCoy-MacMini.local>
2016-07-14 08:11:30 -0700

    Finished adding 3 websites to be loaded when button is pressed
    I removed the status bar at the top of the iPhone screen because I
    thought the app looks better without it.
    I also added three websites incl a prank video lol.

--- webRequest Exercise/ViewController.swift
+++ webRequest Exercise/ViewController.swift
@@ -22,12 +22,13 @@ class ViewController: UIViewController {
         webView = WKWebView()
         container.addSubview(webView)
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let frame = CGRect(x: 0, y: 0, width: container.bounds.width, height: container.bounds.height)
         webView.frame = frame
         
-        let urlStr = ""
-        let url = URL(string: urlStr)
-        let request = NSURLRequest(url: url!)
+        loadRequest(urlStr: "http://www.vox.com/2016/7/12/12159304/pokemon-go-game-ios-android")
         
     }
 
@@ -35,7 +36,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func loadRequest(urlStr: String) {
+        let url = URL(string: urlStr)
+        let request = NSURLRequest(url: url!)
+        webView.load(request as URLRequest)
+    }
+    
+    @IBAction func loadPortfolio(_ sender: AnyObject) {
+        loadRequest(urlStr: "http://www.McCoyGames.com")
+    }
+    
+    @IBAction func loadMyBlog(_ sender: AnyObject) {
+        loadRequest(urlStr: "http://NerdOrGeek.com")
+    }
+    
+    @IBAction func loadPrankVideo(_ sender: AnyObject) {
+        loadRequest(urlStr: "https://www.youtube.com/watch?v=5zk5JriNbZc")
+    }
+    
+    
+    /* Hide Status Bar */
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    /* End Hide Status Bar */
+    
 }
 
